903f7d99c16a39dc22c34208b77b99b7f296af89
Максик <sharapovm505@gmail.com>
2022-12-28 20:26:50 +0300

    с14 новое

--- 14.py
+++ 14.py
@@ -5,3 +5,9 @@ while x != 0:
     x //= 2
 s = s[::-1]
 print(s.count("1"))
+'''for x in '012345678':
+    for y in '012345678':
+        a = int('88' + x + '4' + y, 9) + int('7' + x + '44' + y, 11)
+        if a % 61 == 0:
+            print(a // 61)
+            break'''
