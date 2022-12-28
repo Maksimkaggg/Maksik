77ffeb9abbffed756e97fa11abe9d6f3d475331f
Максик <sharapovm505@gmail.com>
2022-12-28 20:27:50 +0300

    8 новое

--- 8.py
+++ 8.py
@@ -6,3 +6,19 @@ for x in product('АНТИУТОПИЯ',repeat=16):
     if :
         k+=1
 print(k)
+'''from itertools import product
+c = 0
+for x in product('ЛЕТО', repeat = 4):
+    s = ''.join(x)
+    if s[0] == 'Л' or s[0] == 'Т':
+        c += 1
+print(c)'''
+
+from itertools import product
+c = 0
+for x in product('АКРУ', repeat = 5):
+    s = ''.join(x)
+    c += 1
+    if c == 350:
+        print(s)
+        break
