6a223b7cc94fd7f9f01ac64f9679ae6d2c294252
sotnikova <13120515@mail.ru>
2023-03-07 15:22:22 +0300

    само простое 12 с решу егэ

--- 12.py
+++ 12.py
@@ -1,5 +1,6 @@
-s = '8' * 68
-while '222' in s or '888' in s:
-        s = s.replace('222', '8', 1)
-        s = s.replace('888', '2', 1)
+s = '1' +'8' * 80
+while '18' in s or '3888' in s:
+        s = s.replace('18', '2', 1)
+        s = s.replace('288', '3', 1)
+        s = s.replace('3888', '1', 1)
 print(s)
